07f85923bed82db4bd09fc994435fddedbc4c3f1
Sudip-Chel <118715456+Sudip-Chel@users.noreply.github.com>
2025-02-11 08:50:25 +0530

    Update multiple_disease_predi.py

--- multiple_disease_predi.py
+++ multiple_disease_predi.py
@@ -23,7 +23,7 @@ working_dir = os.path.dirname(os.path.abspath(__file__))
 # loading the saved models
 
 diabetes_model = pickle.load(open(f'{working_dir}/saved_models/diabetes_model.sav','rb'))
-heart_disease_model = pickle.load(open(f'{working_dir}/saved_models/heart_disease_model.sav','rb'))
+heart_disease_model = pickle.load(open(f'{working_dir}/saved_models/heart_model.sav','rb'))
 
 # sidebar for navigation
 with st.sidebar:
